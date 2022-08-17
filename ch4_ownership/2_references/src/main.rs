f93506811a5b214a91042be6efc10524a3c7396a
maxestorr <maxestorr@gmail.com>
2022-08-17 22:15:38 +0100

    Mutable references

---
+++ ch4_ownership/2_references/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    fun_1();
+    fun_2();
+}
+
+fn fun_1() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn fun_2() {
+    let mut s = String::from("hello");
+
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
