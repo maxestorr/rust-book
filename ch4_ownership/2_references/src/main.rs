d08123536ea4437efd17024ab78a7b925e3b8fbf
maxestorr <maxestorr@gmail.com>
2022-08-17 22:30:25 +0100

    Squash me later

--- ch4_ownership/2_references/src/main.rs
+++ ch4_ownership/2_references/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     fun_1();
     fun_2();
+    fun_3();
 }
 
 fn fun_1() {
@@ -23,4 +24,18 @@ fn fun_2() {
 
 fn change(some_string: &mut String) {
     some_string.push_str(", world");
+}
+
+fn fun_3() {
+    let mut s = String::from("hello");
+
+    // let r1 = &mut s; // Cannot have multiple mutable references like this
+    // let r2 = &mut s; 
+
+    // Use curly brace scopes to allow for this, which prevents simultaneous mutable references
+    {
+        let r1 = &mut s;
+    }
+    let r2 = &mut s; 
+    
 }
